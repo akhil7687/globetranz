05c987a9ba9e7cb6b7599c6606dcd70a72383c63
AKHIL <puru@suphalaam.com>
2018-11-14 16:31:23 +0530

    globetranz

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,35 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery-1.12.4.min
+//= require bootstrap.min
+//= require jquery.fancybox.pack
+//= require jquery.fancybox-media
+//= require isotope.pkgd.min
+//= require owl
+//= require jquery.appear
+//= require jquery.countTo
+//= require validate
+//= require jquery.themepunch.tools.min
+//= require jquery.themepunch.revolution.min
+//= require revolution.extension.actions.min
+//= require revolution.extension.carousel.min
+//= require revolution.extension.kenburn.min
+//= require revolution.extension.layeranimation.min
+//= require revolution.extension.migration.min
+//= require revolution.extension.navigation.min
+//= require revolution.extension.parallax.min
+//= require revolution.extension.slideanims.min
+//= require revolution.extension.video.min
+//= require scripts.min
+//= require scripts
+//= require turbolinks
